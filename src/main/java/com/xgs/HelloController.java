a76ca221cd0c1b2b42951f13fb03f88622a958cc
nilzxq <2013358717@qq.com>
2017-08-08 15:44:56 +0800

    yml

--- src/main/java/com/xgs/HelloController.java
+++ src/main/java/com/xgs/HelloController.java
@@ -1,5 +1,6 @@
 package com.xgs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	@Value("${cupSize}")
+	private String cupSize;
+	
+	@Value("${age}")
+	private Integer age;
+	
+	@Value("${content}")
+	private String content;
+	
 	@RequestMapping(value="/hello",method=RequestMethod.GET)
 	public String say() {
-		return "Hello Spring Boot!";
+		return content;
 	}
 }
