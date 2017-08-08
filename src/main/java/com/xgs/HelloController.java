b733ccae4dd1bfdbff4fb2cb56b7e62537104d93
nilzxq <2013358717@qq.com>
2017-08-08 17:12:50 +0800

    controller

--- src/main/java/com/xgs/HelloController.java
+++ src/main/java/com/xgs/HelloController.java
@@ -1,7 +1,9 @@
 package com.xgs;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,8 +11,11 @@ public class HelloController {
 
 	private GrilProperties grilProperties;
 	
-	@RequestMapping(value="/hello",method=RequestMethod.GET)
-	public String say() {
-		return grilProperties.getCupSize();
+//	@RequestMapping(value= {"/hello","/hi"},method=RequestMethod.GET)
+	//组合注解
+	@GetMapping(value="/hello")
+	public String say(@RequestParam(value="id",required=false,defaultValue="0") Integer myId) {
+		return "id: "+myId;
+//		return grilProperties.getCupSize();
 	}
 }
