8ead976f6d07b1dbe24862baeaaa7295ef2f705a
nilzxq <2013358717@qq.com>
2017-08-09 16:21:03 +0800

    logger

--- src/main/java/com/xgs/aspect/HttpAspect.java
+++ src/main/java/com/xgs/aspect/HttpAspect.java
@@ -4,12 +4,15 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class HttpAspect {
 	
+	private static final Logger log = LoggerFactory.getLogger(HttpAspect.class);
 	@Pointcut("execution(public * com.xgs.controller.GrilController.*(..))")
 	public void log() {
 		
@@ -17,11 +20,11 @@ public class HttpAspect {
 	
 	@Before("log()")
 	public void doBefore() {
-		System.out.println("1111111111111111111");
+		log.info("1111111111111111111");
 	}
 	@After("log()")
 	public void doAfter() {
-		System.out.println("22222222");
+		log.info("222222222222");
 	}
 
 }
