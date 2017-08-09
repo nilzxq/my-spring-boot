54319ea252f73c0926bd94de6686214f55b12b39
nilzxq <2013358717@qq.com>
2017-08-09 16:15:37 +0800

    after

--- src/main/java/com/xgs/aspect/HttpAspect.java
+++ src/main/java/com/xgs/aspect/HttpAspect.java
@@ -1,16 +1,27 @@
 package com.xgs.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class HttpAspect {
 	
-	@Before("execution(public * com.xgs.controller.GrilController.*(..))")
+	@Pointcut("execution(public * com.xgs.controller.GrilController.*(..))")
 	public void log() {
+		
+	}
+	
+	@Before("log()")
+	public void doBefore() {
 		System.out.println("1111111111111111111");
+	}
+	@After("log()")
+	public void doAfter() {
+		System.out.println("22222222");
 	}
 
 }
